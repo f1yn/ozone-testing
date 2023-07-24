9e55808369f9a53c8b7fa70b2d78d76283f2f5e0
Flynn <gpg@flyn.ca>
2023-07-23 21:43:42 -0400

    Continued work.

--- service-a/runtime.ts
+++ service-a/runtime.ts
@@ -1,7 +1,11 @@
-import { createService } from '/ozone/service-deno/service.ts';
+import { createService, addListener } from '@o3/service-deno/service.ts';
 
 const serverToBeReady = createService();
 
 await serverToBeReady;
+
+addListener('SOME_INTENT', () => {
+    console.log("Received SOME_INTENT")
+})
 
 console.log('Server is ready');
--- service-b/runtime.ts
+++ service-b/runtime.ts
@@ -1,7 +1,12 @@
-import { createService } from '/ozone/service-deno/service.ts';
+import { createService, send } from '@o3/service-deno/service.ts';
 
 const serverToBeReady = createService();
 
 await serverToBeReady;
 
 console.log('Server is ready');
+
+// Perform repetitive action because why not
+setInterval(() => {
+    send('SOME_INTENT', { whoop: true })
+}, 1000)
